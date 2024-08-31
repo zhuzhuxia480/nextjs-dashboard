0f07d55a22a472f27f2fdaf56142e4374b462a84
zhuzhuxia <742491842@qq.com>
2024-08-31 16:33:24 +0800

    change monitor impl

--- app/lib/task.js
+++ app/lib/task.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import pako from "pako";
 import async from "./sendEmail"
+import logger from "./logger";
 
 const reqUrl = "https://mcpwxp.motherchildren.com/cloud/appointment/publicClient/selDoctorListPagingBiz"
 const userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.50(0x1800323a) NetType/WIFI Language/zh_CN"
@@ -28,7 +29,7 @@ async function Task() {
         }
 
         const result = await response.json();
-        console.log('Success:', result);
+        logger.info('Success:', result);
         await dealWithResult(result)
     } catch (error) {
         console.error('Error:', error);
@@ -38,8 +39,8 @@ async function Task() {
 function isBetween08() {
     const now = new Date();
     const hours = now.getHours();
-    console.log(hours);
-    return hours > 0 && hours <= 8;
+    logger.info(hours);
+    return hours > 0 && hours < 8; //TODO
 }
 
 function get14DaysAfter() {
@@ -52,7 +53,7 @@ function get14DaysAfter() {
     const day = String(futureDate.getDate()).padStart(2, '0');
 
     const formattedDate = `${year}-${month}-${day}`;
-    console.log(formattedDate); // 输出类似 "2024-09-02" 的格式
+    logger.info(formattedDate); // 输出类似 "2024-09-02" 的格式
     return formattedDate;
 }
 
@@ -77,24 +78,28 @@ async function getDocSchedule(docId) {
         }
 
         const result = await response.json();
-        console.log('req doc schedule:', result);
+        logger.info('req doc schedule:', result);
         return result.data.scheduleRespVo.scheduleRespVos;
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 //{docId, scheduleRespVos[]}
 async function getAllDocDate(docs) {
     let docsSchedule = [];
     for (const doc of docs) {
-        let docSchedule;
+        let docSchedule = {};
         docSchedule.docId = doc.doctorId;
+        docSchedule.name = doc.doctorName;
         let schedule = await getDocSchedule(doc.doctorId);
         if (schedule && schedule.length > 0) {
-            docSchedule.scheduleRespVos = schedule;
+            docSchedule.scheduleRespVos = schedule.filter(value => value.statusName === "有号");
             docsSchedule.push(docSchedule)
         }
+        await delay(1000);
     }
     return docsSchedule;
 }
@@ -103,19 +108,36 @@ function scheduleOnlyAfter14d(docsSchedule) {
 
 }
 
+async function getDocsNameWithSchedule(docs) {
+    let ret = [];
+    let between08 = isBetween08();
+    let _14DaysAfter = get14DaysAfter();
+
+    if (between08) {
+        // let docsSchedule = await getAllDocDate(docs);
+        // for (const doc of docsSchedule) {
+        //     doc.scheduleRespVos = doc.scheduleRespVos.filter((schedule) => schedule.fullScheduleDate !== _14DaysAfter);
+        //     if (doc.scheduleRespVos.length > 0) {
+        //         ret.push(doc.name);
+        //     }
+        // }
+    } else {
+        ret = docs.map(value => value.doctorName);
+    }
+    return ret;
+}
+
 //每天0-8点之间：不检查第14天
 //其他时间：检查所有
 async function dealWithResult(result) {
     if (result.data.hxPageDoctorList.content.length > 0) {
-        //all doc`s schedule
-        let docsSchedule = await getAllDocDate(result.data.hxPageDoctorList.content);
-        console.log("get data", docsSchedule);
-        if (isBetween08()) {
 
+        let docsName = await getDocsNameWithSchedule(result.data.hxPageDoctorList.content);
+         if (docsName.length > 0) {
+             logger.info(JSON.stringify(docsName));
+             await async({to: "742491842@qq.com,lihaoggb@gmail.com", subject: "当前有号医生", text: docsName.join(",")});
         }
-        await async({to: "742491842@qq.com,lihaoggb@gmail.com", subject: "test email", text: JSON.stringify(result)});
     }
-
 }
 
 
